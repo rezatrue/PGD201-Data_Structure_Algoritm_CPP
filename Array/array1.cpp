4d7a5f92cf104e3c717af037f90ce979ee2bfb81
rezatrue <rezatrue@gmail.com>
2018-07-02 00:53:35 +0600

    local array

---
+++ Array/array1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int number = 10;
+	/* local array 
+	-Created in stack memory
+	-Has limited life time, exist with in scope/ funtion
+	-Must be small/ limited in size, as stack is limited for each application
+	-High level of sequrity
+	-Exclusive
+	-if overflowed application crashed [segmentation error]	
+	*/
+	int arrL[number];
+
+	for (int i = 0; i < number ; i++)
+	{
+		arrL[i] = (i+1)*10;
+	}
+
+	for(int i = 0; i < number ; i++)
+	{
+		cout << arrL[i] << endl;
+	}
+
+	return 0;
+
+}
