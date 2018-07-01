88b9576593d912bf651d2c30d649b1cfbd2101e6
rezatrue <rezatrue@gmail.com>
2018-07-02 00:54:09 +0600

    local array issue

---
+++ Array/array1.1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int* tain(int* arrL)
+{
+	cout << arrL[5] << endl;
+	int arrT[3] = {1,2,3};
+	return arrT;
+}
+
+
+int main(void)
+{
+	int number = 10;
+	/* local array 
+	-Created in stack memory
+	-Has limited life time, exist with in scope/ funtion
+	-Must be small/ limited in size, as stack is limited for each application
+	-High level of sequrity
+	-Exclusive
+	-if overflowed application crashed [segmentation error]	
+	*/
+	int arrL[number];
+
+	for (int i = 0; i < number ; i++)
+	{
+		arrL[i] = (i+1)*10;
+	}
+
+	for(int i = 0; i < number ; i++)
+	{
+		cout << arrL[i] << endl;
+	}
+
+
+	// value of the location array arrT don't exist
+	// segmentation fault (core dumped)
+	int* arr = tain(arrL);
+	// unable to print output
+	cout << arr[1] << endl;
+
+	return 0;
+
+}
