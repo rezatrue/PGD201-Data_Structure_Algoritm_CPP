7b456f4ab900c38891c9f6e3f722df4a2442d4c4
rezatrue <rezatrue@gmail.com>
2018-07-02 07:04:19 +0600

    Global vaiable issue

---
+++ Array/array3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// problem with global vaiable
+/*
+as value of n is not assigned, 
+array created with the garvadge value of n
+so size become unknown
+*/
+
+int n;
+int arrG[n];
+
+
+int main (void)
+{
+	cout << "point 1" << endl;
+	n = 4096*4096;
+	cout << "point 2" << endl;
+	for (int i = 0; i < n ; i++) arrG[i] = i;
+	cout << "point 3" << endl;
+
+	return 0;
+}
