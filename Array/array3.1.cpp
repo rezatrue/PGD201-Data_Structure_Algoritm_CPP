12252dc93ba1016eb6098874ae0289f03a829ee6
rezatrue <rezatrue@gmail.com>
2018-07-02 07:05:20 +0600

    Local vaiable size issue

---
+++ Array/array3.1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// problem with Local vaiable
+/*
+segmentation fault (core dumped) due to size
+*/
+
+
+
+int main (void)
+{
+	
+	int n;
+
+	cout << "point 1" << endl;
+	n = 4096*4096;
+	cout << "point 2" << endl;
+	int arrL[n];	
+	for (int i = 0; i < n ; i++) arrL[i] = i;
+	cout << "point 3" << endl;
+
+	return 0;
+}
